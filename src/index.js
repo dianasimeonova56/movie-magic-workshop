ad8cb4b259a9d6a13078cbe37be46c1ab4a8f6dd
dianasim <diana.simeonova56@gmail.com>
2025-05-25 22:57:12 +0300

    add and config express handlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import express from "express"
+import handlebars from "express-handlebars"
 
+//init express instance
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("it works")
-})
+//add and config view engine
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
 
+//set default engine
+app.set('view engine', 'hbs');
+
+//config routes
+app.get('/', (req, res) => {
+    res.render('home', {layout:false});
+});
+
+//start express server
 app.listen(5000, () => {
     console.log("Server is listening on http://localhost:5000...");
 })
