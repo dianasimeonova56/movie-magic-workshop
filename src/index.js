82cb9890e95565b2735569c48f666e05ba0705bd
dianasim <diana.simeonova56@gmail.com>
2025-05-25 22:52:49 +0300

    add express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-console.log(
-    "az"
-);
+import express from "express"
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("it works")
+})
+
+app.listen(5000, () => {
+    console.log("Server is listening on http://localhost:5000...");
+})
