13826f6d4c7ed2c891bb71e3b63c8efb91259348
dianasim <diana.simeonova56@gmail.com>
2025-06-02 22:53:58 +0300

    create mongoose model Movie

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,25 +1,16 @@
-import { log } from 'node:console';
-import fs from 'node:fs/promises'
-import { v4 as uuid } from 'uuid'
+import {Schema, model} from "mongoose";
 
-const movieJson = await fs.readFile('./src/database.json');
-export const movies = JSON.parse(movieJson);
+const movieSchema = new Schema({
+    title: String,
+    category: String, 
+    genre: String,
+    director: String,
+    year: Number,
+    imageUrl: String,
+    rating: Number,
+    description: String
+})
 
+const Movie = model("Movie", movieSchema);
 
-export default class Movie {
-    constructor(data) {
-        this.data = data;
-    }
-
-    async save() {
-        this.data.id = uuid();
-        //movieData.id = uuid();
-        this.data.rating = Number(this.data.rating);
-
-        movies.push(this.data);
-
-        await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
-
-        return this.data;
-    }
-}
+export default Movie;
