9b5e940910accec765261aa1381acb430e2ba022
dianasim <diana.simeonova56@gmail.com>
2025-06-03 14:54:05 +0300

    add cast

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,9 +1,19 @@
 import {Router} from "express"
+import castService from "../services/castService.js";
 
 const castController = Router();
 
 castController.get('/create', (req, res)=> {
     res.render('cast/create'); //from the folder cast -> create
+})
+
+castController.post('/create', async (req, res)=> {
+   const castData = req.body;
+
+   // send to service?
+   await castService.create(castData);
+   
+   res.redirect('/');
 })
 
 export default castController;
---
+++ src/services/castService.js
@@ -0,0 +1,8 @@
+import Cast from '../models/Cast.js';
+
+export default {
+    create(castData) {
+        return Cast.create(castData); // returns a promise form the document we will create; ONLY return the reslut/promise
+        // it will be awaited in the caller of the func
+    }
+}
