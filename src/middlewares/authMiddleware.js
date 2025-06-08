70ab64db3c4ea18b4852ce3be20c0c781786a421
dianasim <diana.simeonova56@gmail.com>
2025-06-08 23:20:14 +0300

    dynamic nav

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -12,6 +12,10 @@ export const auth = (req, res, next) => {
 
         req.user = { id, email }; // attach to the request with new info
 
+        // delegated space that can be injected into the context of view engines
+        //temporary for the life of the current request
+        res.locals.user = { id, email }; // this way we can check for the dynamic nav of we are logged in
+
         next();
     } catch (err) {
         res.clearCookie('auth'); // if its invalid cookie, we should clear it
