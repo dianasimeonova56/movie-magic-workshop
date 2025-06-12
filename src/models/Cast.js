a04935b05b530961118992e709e48a613a3c087d
dianasim <diana.simeonova56@gmail.com>
2025-06-12 22:08:18 +0300

    add movie and cast validation

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,26 +1,32 @@
-import {Schema, model} from "mongoose"
+import { Schema, model } from "mongoose"
+
+const validCharsPattern = /^[a-zA-Z0-9 ]+$/;
 
 const castSchema = new Schema({
-    name:{
+    name: {
         type: String,
         required: true,
+        validate: [validCharsPattern, 'Invalid name format! Only english letters, digits and whitespaces are allowed'],
+        minLength: [5, "Should be at least 5 chars long!"],
     },
     age: {
         type: Number,
         required: true,
-        min: 12,
-        max: 120,
-    }, 
+        min: [1, 'Age should be at least 1 year old'],
+        max: [120, "Age should be less than 120 years old"],
+    },
     born: {
-        type: String, 
+        type: String,
         required: true,
-    }, 
+        validate: [validCharsPattern, 'Invalid title format! Only english letters, digits and whitespaces are allowed'],
+        minLength: [10, "Born be at least 5 chars long!"],
+    },
     imageUrl: {
         type: String,
         required: true,
         validate: [/^https?:\/\//, 'Invalid ImageUrl!'],
     },
-    
+
 })
 
 const Cast = model("Cast", castSchema);
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,28 +1,37 @@
-import {Schema, model, Types} from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
 const maxYearAllowed = new Date().getFullYear() + 5;
+const validCharsPattern = /^[a-zA-Z0-9 ]+$/;
 const movieSchema = new Schema({
-    title:  {
+    title: {
         type: String,
         required: [true, "Field title is required!"],
+        validate: [validCharsPattern, 'Invalid title format! Only english letters, digits and whitespaces are allowed'],
+        minLength: [5, "Should be at least 5 chars long!"],
     },
     category: {
-        type: String, 
+        type: String,
         required: [true, "Field category is required!"],
+        validate: [validCharsPattern, 'Invalid category format! Only english letters, digits and whitespaces are allowed'],
+        minLength: [5, "Should be at least 5 chars long!"],
     },
     genre: {
         type: String,
         required: [true, "Field genre is required!"],
         lowercase: true, //not a validator, but a sanitizer -> it will make it lowercase
+        validate: [validCharsPattern, 'Invalid genre format! Only english letters, digits and whitespaces are allowed'],
+        minLength: [5, "Should be at least 5 chars long!"],
     },
     director: {
         type: String,
-       required: [true, "Field director is required!"],
+        required: [true, "Field director is required!"],
+        validate: [validCharsPattern, 'Invalid director format! Only english letters, digits and whitespaces are allowed'],
+        minLength: [5, "Should be at least 5 chars long!"],
     },
     year: {
         type: Number,
         required: [true, "Field year is required!"],
-        min: 1970,
+        min: [1990, "Year cannot be lower than 1990!"],
         max: [maxYearAllowed, `Year cannot be  larger than ${maxYearAllowed}`],
     },
     imageUrl: {
@@ -31,7 +40,7 @@ const movieSchema = new Schema({
         validate: [/^https?:\/\//, 'Invalid ImageUrl!'],
     },
     rating: {
-       type: Number,
+        type: Number,
         required: [true, "Field rating is required!"],
         min: [1, "Rating cannot be lower than 1"],
         max: [10, "Rating should not be higher than 10!"],
@@ -39,6 +48,8 @@ const movieSchema = new Schema({
     description: {
         type: String,
         required: [true, "Field description is required!"],
+        minLength: [20, 'Description should be at least 20 chars'],
+        validate: [validCharsPattern, 'Invalid title format! Only english letters, digits and whitespaces are allowed'],
         maxLength: [1000, "Description is too long!"],
     },
     //reference; array with object ids that refer to the Cast model
